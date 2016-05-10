679512b05639fba9226d444907adbe9dff1c5188
Tracy <tmcook23@aol.com>
2016-05-09 20:49:48 -0500

    Fixes to Final Project Scraper

--- FinalProjectScraper.py
+++ FinalProjectScraper.py
@@ -3,56 +3,46 @@
 import urllib2, csv, mechanize
 from bs4 import BeautifulSoup
 
-output = open('FinalWeather.csv', 'w')
+output = open('FinalWeather_v2.csv', 'w')
 writer = csv.writer(output)
-
-br = mechanize.Browser() #set up
-html = urllib2.urlopen('http://www.columbiamissourian.com/weather/?weather_zip=65201').read()
-
-print html
 
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.open('http://www.columbiamissourian.com/weather/?weather_zip=65201') #performs action
 
-
 html = br.response().read()
 soup = BeautifulSoup(html, "html.parser")
-
 
 #Submit form
-br.select_form(nr=1) #fills out form
-br.form['form-control'] = ['65201']
+#br.select_form(nr=3) #fills out form
 #br.form['weather_zip'] = ['65201']
-br.submit('btn btn-default')
+#br.submit('btn btn-default')
 #br.submit('weather_zip') #submits form
 
-
-html = br.response().read()
-soup = BeautifulSoup(html, "html.parser")
-
-
-weather = soup.find('select',
-	{'id': 'weather-refresh-ico',
-	'class': 'fa fa-refresh'
+weather = soup.find('div',
+	{'class': 'weather-right-now-temp',
+	#'class': 'fa fa-refresh'
 	#'class': 'weather-right-now-details'
 })
 
-print output
+print weather
+
+for x in weather:
+	if x.string:
+		print 'Temperature right now: ' + x.string.strip()
 
 #weather_values = weather.find('option')
 
-for temp in 'weather-right-now-temp':
-	temp_value = temp.text
-	print temp['value']
-	###county_id = county['value']
+#for temp in 'weather-right-now-temp':
+#	temp_value = temp.text
+#	print temp['value']
 
-	main_table = soup.find('table',
-		{'class': 'weather-now-container'
-	})
+#	main_table = soup.find('table',
+#		{'class': 'weather-now-container'
+#	})
 
-	print main_table
-	print output
-	writer.writerow(data)
+#	print main_table
+#	print output
+#	writer.writerow(data)
         
 # end of file
